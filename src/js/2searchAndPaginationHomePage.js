51625107c1c3be9d97ac720c51d1fd4b1472a01c
RomanKonopelko <romank761@gmail.com>
2021-01-28 18:57:12 +0200

    create api constructor and fetch

--- src/js/2searchAndPaginationHomePage.js
+++ src/js/2searchAndPaginationHomePage.js
@@ -1,19 +1,14 @@
 const API_KEY = '91085a172e1ffb2047d72641d0a91356';
-const IMAGE_URL = 'https://image.tmdb.org/t/p/w300';
 
-const movieSearch = function (query) {
-  return fetch(
-    `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&per_page=10`,
-  )
-    .then(data => data.json())
-    .then(({ results }) => {
-      console.dir(results);
-      results.forEach(res => {
-        console.log(res);
-        ul.append(createFilmCard(res));
-      });
-    });
-};
+const MyApi = new MovieApi(API_KEY);
+
+const ul = document.querySelector('.test');
+console.log(ul);
+MyApi.movieSearch().then(results =>
+  results.forEach(el => {
+    return ul.append(createFilmCard(el));
+  }),
+);
 
 form.addEventListener('submit', e => {
   e.preventDefault();
@@ -23,7 +18,6 @@ form.addEventListener('submit', e => {
 });
 
 //ТЕСТОВЫЙ СПИСОК
-const ul = document.querySelector('.test');
 
 // ТЕСТОВАЯ ОТРИСОВКА. ТРЕБУЮТСЯ ГОТОВЫЙ ШАБЛОН КАРТЫ С КЛАССАМИ
 const createFilmCard = function (arr) {
@@ -34,7 +28,7 @@ const createFilmCard = function (arr) {
 
   description.textContent = arr.overview;
   name.textContent = arr.name || arr.title;
-  mainPic.src = IMAGE_URL + arr.backdrop_path;
+  mainPic.src = MyApi.IMAGE_BASE_URL + arr.backdrop_path;
   li.append(name, mainPic, description);
   return li;
 };
