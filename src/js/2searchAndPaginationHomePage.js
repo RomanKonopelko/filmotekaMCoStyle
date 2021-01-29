96a35073cb5bcc2a79a6b1a057fda5968dcafe7f
RomanKonopelko <romank761@gmail.com>
2021-01-29 12:00:06 +0200

    add pagination logic

--- src/js/2searchAndPaginationHomePage.js
+++ src/js/2searchAndPaginationHomePage.js
@@ -5,68 +5,9 @@ const MyApi = new MovieApi(API_KEY, paginationWrapper, ul);
 
 form.addEventListener('submit', e => {
   e.preventDefault();
+  MyApi.resetPage();
   let inputValue = e.target.elements.query.value;
   MyApi.params.query = inputValue;
-  MyApi.movieSearch()
-    .then(data => {
-      console.log(data.total_pages);
-      MyApi.checkPaginationRatio(data);
-      MyApi.setRatioButtons(data);
-      return data;
-    })
-    .then(({ results }) =>
-      results.forEach(el => {
-        return MyApi.pagination.paginationContainer.append(
-          MyApi.createFilmCard(el),
-        );
-      }),
-    );
-});
-
-//ТЕСТОВЫЙ СПИСОК
-
-// ТЕСТОВАЯ ОТРИСОВКА. ТРЕБУЮТСЯ ГОТОВЫЙ ШАБЛОН КАРТЫ С КЛАССАМИ
-
-// function pagination(data) {
-//   const totalPages = data.total_pages;
-//   let btnArr = [];
-//   for (let i = 1; i <= totalPages; i++) {
-//     const paginationBtn = document.createElement('button');
-//     paginationBtn.addEventListener('click', e => {
-//       ul.innerHTML = '';
-//       console.log(e.target.textContent);
-//       MyApi.params._page = +e.target.textContent;
-//       MyApi.movieSearch().then(({ results }) =>
-//         results.forEach(el => {
-//           return ul.append(createFilmCard(el));
-//         }),
-//       );
-//     });
-//     paginationBtn.textContent = i;
-//     console.log(i);
-//     btnArr.push(paginationBtn);
-//   }
-//   console.dir(btnArr);
-//   return paginationWrapper.append(...btnArr);
-// }
-
-btnNext.addEventListener('click', () => {
-  MyApi.incrementPage();
-  ul.innerHTML = '';
-  MyApi.movieSearch().then(({ results }) =>
-    results.forEach(el => {
-      return ul.append(createFilmCard(el));
-    }),
-  );
-});
-
-btnPrev.addEventListener('click', () => {
-  MyApi.decrementPage();
-  ul.innerHTML = '';
-  console.log(MyApi.params._page);
-  MyApi.movieSearch().then(({ results }) =>
-    results.forEach(el => {
-      return ul.append(createFilmCard(el));
-    }),
-  );
+  MyApi.movieSearch();
+  form.reset();
 });
