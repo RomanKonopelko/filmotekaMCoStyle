9d83b2249369458ce0507e706913b415bce8a7c5
Evgeniy Chernyshenko <magdegaben@gmail.com>
2021-01-29 21:43:49 +0200

    adding changes to img calculation and adding classes

--- src/js/headerLogoText.js
+++ src/js/headerLogoText.js
@@ -1,10 +1,10 @@
-const refs = {
-        logoText: document.querySelector('[data-logoText]'),
-    
-};
-if (document.body.clientWidth <= 320) {
-    refs.logoText.classList.add("is-hiden")
-}
-else {
-    refs.logoText.classList.remove("is-hiden")
-}
+// const refs = {
+//   logoText: document.querySelector('[data-logoText]'),
+// };
+
+// console.log(refs.logoText);
+// if (document.body.clientWidth <= 320) {
+//   refs.logoText.classList.add('is-hidden');
+// } else {
+//   refs.logoText.classList.remove('is-hidden');
+// }
--- src/js/navigationEfectSwitch.js
+++ src/js/navigationEfectSwitch.js
@@ -1,20 +1,20 @@
-(() => {
-    const refs = {
-          iconButton: document.querySelector('[data-icon]'),
-    homeButton: document.querySelector('[data-home]'),
-    libraryButton: document.querySelector('[data-library]'),
-    // modal: document.querySelector('[data-modal]'),
-  };
+// (() => {
+//     const refs = {
+//           iconButton: document.querySelector('[data-icon]'),
+//     homeButton: document.querySelector('[data-home]'),
+//     libraryButton: document.querySelector('[data-library]'),
+//     // modal: document.querySelector('[data-modal]'),
+//   };
 
-    refs.homeButton.addEventListener('click', homeEfect);
-    refs.iconButton.addEventListener('click', homeEfect);
-  refs.libraryButton.addEventListener('click', libraryEfect);
+//     refs.homeButton.addEventListener('click', homeEfect);
+//     refs.iconButton.addEventListener('click', homeEfect);
+//   refs.libraryButton.addEventListener('click', libraryEfect);
 
-  function homeEfect() {
-    refs.homeButton.classList.add('current');
+//   function homeEfect() {
+//     refs.homeButton.classList.add('current');
 
-    }
-    function libraryEfect() {
-        refs.libraryButton.classList.toggle('current');
-    }
-})();
+//     }
+//     function libraryEfect() {
+//         refs.libraryButton.classList.toggle('current');
+//     }
+// })();
