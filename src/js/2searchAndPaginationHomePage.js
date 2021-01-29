6e3657f2dd3bca6b9ae3fbbec02f1d5a48655785
RomanKonopelko <romank761@gmail.com>
2021-01-29 02:00:12 +0200

    extend api constructor and improves pagination

--- src/js/2searchAndPaginationHomePage.js
+++ src/js/2searchAndPaginationHomePage.js
@@ -1,8 +1,7 @@
 const API_KEY = '91085a172e1ffb2047d72641d0a91356';
 
-const MyApi = new MovieApi(API_KEY);
-
 const ul = document.querySelector('.test');
+const MyApi = new MovieApi(API_KEY, paginationWrapper, ul);
 
 form.addEventListener('submit', e => {
   e.preventDefault();
@@ -11,12 +10,15 @@ form.addEventListener('submit', e => {
   MyApi.movieSearch()
     .then(data => {
       console.log(data.total_pages);
-      pagination(data);
+      MyApi.checkPaginationRatio(data);
+      MyApi.setRatioButtons(data);
       return data;
     })
     .then(({ results }) =>
       results.forEach(el => {
-        return ul.append(createFilmCard(el));
+        return MyApi.pagination.paginationContainer.append(
+          MyApi.createFilmCard(el),
+        );
       }),
     );
 });
@@ -24,51 +26,33 @@ form.addEventListener('submit', e => {
 //ТЕСТОВЫЙ СПИСОК
 
 // ТЕСТОВАЯ ОТРИСОВКА. ТРЕБУЮТСЯ ГОТОВЫЙ ШАБЛОН КАРТЫ С КЛАССАМИ
-const createFilmCard = function (arr) {
-  const li = document.createElement('li');
-  const description = document.createElement('p');
-  const name = document.createElement('h1');
-  const mainPic = document.createElement('img');
-  mainPic.width = 300;
 
-  description.textContent = arr.overview;
-  name.textContent = arr.name || arr.title;
-  mainPic.src = arr.backdrop_path
-    ? MyApi.IMAGE_BASE_URL + arr.backdrop_path
-    : MyApi.DEFAULT_IMAGE;
-  li.append(name, mainPic, description);
-  return li;
-};
-
-function pagination(data) {
-  const totalPages = data.total_pages;
-  let btnArr = [];
-  for (let i = 1; i <= totalPages; i++) {
-    const paginationBtn = document.createElement('button');
-    paginationBtn.addEventListener('click', e => {
-      ul.innerHTML = '';
-      console.log(e.target.textContent);
-      MyApi.params._page = +e.target.textContent;
-      console.log(MyApi.params._page, 'api');
-      console.log(MyApi.params.query);
-      MyApi.movieSearch().then(({ results }) =>
-        results.forEach(el => {
-          return ul.append(createFilmCard(el));
-        }),
-      );
-    });
-    paginationBtn.textContent = i;
-    console.log(i);
-    btnArr.push(paginationBtn);
-  }
-  console.dir(btnArr);
-  return paginationWrapper.append(...btnArr);
-}
+// function pagination(data) {
+//   const totalPages = data.total_pages;
+//   let btnArr = [];
+//   for (let i = 1; i <= totalPages; i++) {
+//     const paginationBtn = document.createElement('button');
+//     paginationBtn.addEventListener('click', e => {
+//       ul.innerHTML = '';
+//       console.log(e.target.textContent);
+//       MyApi.params._page = +e.target.textContent;
+//       MyApi.movieSearch().then(({ results }) =>
+//         results.forEach(el => {
+//           return ul.append(createFilmCard(el));
+//         }),
+//       );
+//     });
+//     paginationBtn.textContent = i;
+//     console.log(i);
+//     btnArr.push(paginationBtn);
+//   }
+//   console.dir(btnArr);
+//   return paginationWrapper.append(...btnArr);
+// }
 
 btnNext.addEventListener('click', () => {
   MyApi.incrementPage();
   ul.innerHTML = '';
-  console.log(MyApi.params._page);
   MyApi.movieSearch().then(({ results }) =>
     results.forEach(el => {
       return ul.append(createFilmCard(el));
