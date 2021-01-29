af2fecf19bd07d3332a1c7db14d863eccd5dd52b
Evgeniy Chernyshenko <magdegaben@gmail.com>
2021-01-29 15:02:22 +0200

    adding data to class

--- src/js/0videoApi.js
+++ src/js/0videoApi.js
@@ -2,13 +2,13 @@ class MovieApi {
   constructor(key, paginationWrapper, cardWrapper) {
     this.API_KEY = key;
     this.BASE_URL = 'https://api.themoviedb.org/3/';
-    this.IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w300';
+    this.IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
     this.DEFAULT_IMAGE = '../images/image-not-found.jpg';
     this.currentPage;
     this.params = {
       generalSearchUrl: 'search/movie?',
-      popularSearchUrl: '',
-      genreSearchUrl: '',
+      popularSearchUrl: 'movie/popular?',
+      genreSearchUrl: 'genre/movie/list?',
       query: '',
       _page: '',
     };
@@ -19,7 +19,48 @@ class MovieApi {
       maxLeft: this.params._page - Math.floor(this.window / 2),
       maxRight: this.params._page + Math.floor(this.window / 2),
     };
+    this.imgCards = {
+      defaultBackdropImg: '',
+      defaultPosterImg: '',
+      currentSizes: {
+        backdropSize: '',
+        posterSize: '',
+      },
+      backdropSizes: {
+        mobile: 'w342',
+        tablet: 'w500',
+        desktop: 'w780',
+      },
+      posterSizes: {
+        mobile: 'w342',
+        tablet: 'w500',
+        desktop: 'w780',
+      },
+    };
   }
+
+  fetchPopularFilmsList() {
+    return fetch(
+      `${this.BASE_URL}${this.params.popularSearchUrl}api_key=${this.API_KEY}&language=en-US&page=${this.params._page}`,
+    )
+      .then(response => response.json())
+      .then(resp => {
+        this.totalPages = resp.total_pages;
+        this.checkPaginationRatio(resp);
+        this.setRatioButtons(resp);
+        return resp;
+      })
+      .then(({ results }) => results);
+  }
+
+  fetchGenres() {
+    return fetch(
+      `${this.BASE_URL}${this.params.genreSearchUrl}api_key=${this.API_KEY}`,
+    )
+      .then(response => response.json())
+      .then(({ genres }) => genres);
+  }
+
   movieSearch() {
     return fetch(
       `${this.BASE_URL}${this.params.generalSearchUrl}api_key=${this.API_KEY}&language=en-US&query=${this.params.query}&page=${this.params._page}`,
@@ -117,4 +158,42 @@ class MovieApi {
   resetPage() {
     return (this.params._page = 1);
   }
+
+  checkBackdropImgSize() {
+    if (window.innerWidth >= 1200) {
+      this.imgCards.currentSizes.backdropSize = this.imgCards.backdropSizes.desktop;
+      this.imgCards.defaultBackdropImg = './images/default_backdrop.jpeg';
+      return;
+    }
+    if (window.innerWidth >= 768 && window.innerWidth < 1200) {
+      this.imgCards.currentSizes.backdropSize = this.imgCards.backdropSizes.tablet;
+      this.imgCards.defaultBackdropImg = './images/default_backdrop.jpeg';
+      return;
+    }
+    if (window.innerWidth < 768) {
+      this.imgCards.currentSizes.backdropSize = this.imgCards.backdropSizes.mobile;
+      this.imgCards.defaultBackdropImg = './images/default_backdrop.jpeg';
+      return;
+    }
+  }
+
+  checkPosterImgSize() {
+    if (window.innerWidth >= 1200) {
+      this.imgCards.currentSizes.posterSize = this.imgCards.posterSizes.desktop;
+      this.imgCards.defaultPosterImg = './images/default_poster.jpg';
+    }
+    if (window.innerWidth >= 768 && window.innerWidth < 1200) {
+      this.imgCards.currentSizes.posterSize = this.imgCards.posterSizes.tablet;
+      this.imgCards.defaultPosterImg = './images/default_poster.jpg';
+    }
+    if (window.innerWidth < 768) {
+      this.imgCards.currentSizes.posterSize = this.imgCards.posterSizes.mobile;
+      this.imgCards.defaultPosterImg = './images/default_poster.jpg';
+    }
+  }
 }
+
+const API_KEY = '91085a172e1ffb2047d72641d0a91356';
+
+// const ul = document.querySelector('.test');
+const MyApi = new MovieApi(API_KEY, paginationWrapper, ulForCards);
--- src/js/2searchAndPaginationHomePage.js
+++ src/js/2searchAndPaginationHomePage.js
@@ -1,7 +1,7 @@
-const API_KEY = '91085a172e1ffb2047d72641d0a91356';
+// const API_KEY = '91085a172e1ffb2047d72641d0a91356';
 
-const ul = document.querySelector('.test');
-const MyApi = new MovieApi(API_KEY, paginationWrapper, ul);
+// const ul = document.querySelector('.test');
+// const MyApi = new MovieApi(API_KEY, paginationWrapper, ul);
 
 form.addEventListener('submit', e => {
   e.preventDefault();
