932bdbea66c11fce723a1f22469327595b52956b
Kateryna Lutska <ket.lutska@gmail.com>
2021-01-30 18:22:26 +0200

    add detailPage and modal

--- src/js/0refs.js
+++ src/js/0refs.js
@@ -2,12 +2,17 @@ const refs = {
   form: document.querySelector('.main__search-form'),
   paginationWrapper: document.querySelector('.pagination'),
   backdrop: document.querySelector('.backdrop'),
-  modalBtn: document.querySelector('.backdrop__modal__btn'),
-  filmImage: document.querySelector('.details-page__foto'),
-  modalText: document.querySelector('.backdrop__text'),
-  aboutFilmText: document.querySelector('.details-page__text'),
   ulForCards: document.querySelector('.gallery__list'),
+  modalBtn: document.querySelector('.backdrop__modal__btn'),
+  detailsSection: document.querySelector('.details-page'),
 };
 
-const { form, paginationWrapper, ulForCards } = refs;
-console.log(form);
+const {
+  form,
+  paginationWrapper,
+  ulForCards,
+  backdrop,
+  modalBtn,
+  detailsSection,
+} = refs;
+// console.log(form);
