ac91d44bcee1daa255161e89cc3f81503266bb06
Kateryna Lutska <ket.lutska@gmail.com>
2021-01-31 14:22:41 +0200

    add button trailer

--- src/js/0videoApi.js
+++ src/js/0videoApi.js
@@ -277,15 +277,23 @@ class MovieApi {
     buttonFirst.classList.add('button__add', 'first');
     buttonFirst.setAttribute('type', 'submite');
     buttonFirst.textContent = 'add to Watched';
+    buttonFirst.addEventListener('click', this.onWatchedClick);
 
     const buttonSecond = document.createElement('button');
     buttonSecond.classList.add('button__add');
     buttonSecond.setAttribute('type', 'submite');
     buttonSecond.textContent = 'add to queue';
+    buttonSecond.addEventListener('click', this.onQueueClick);
+
+    const buttonTrailer = document.createElement('button');
+    buttonTrailer.classList.add('button__add');
+    buttonTrailer.setAttribute('type', 'submite');
+    buttonTrailer.textContent = 'watched trailer';
+    buttonTrailer.addEventListener('click', this.onTrailerClick);
 
     const divBtn = document.createElement('div');
     divBtn.classList.add('details-page__button');
-    divBtn.append(buttonFirst, buttonSecond);
+    divBtn.append(buttonFirst, buttonSecond, buttonTrailer);
 
     const detailsPageDecr = document.createElement('div');
     detailsPageDecr.classList.add('details-page__description');
@@ -322,8 +330,6 @@ class MovieApi {
     detailsSection.classList.remove('is-hidden');
     detailsSection.appendChild(container);
 
-    detailsSection.addEventListener('click', openModal);
-
     //Затирает карточку после закрытия страницы
 
     btnClose.addEventListener('click', () => {
@@ -333,6 +339,15 @@ class MovieApi {
       ulForCards.classList.remove('is-hidden');
       detailsSection.innerHTML = '';
     });
+  }
+  onTrailerClick() {
+    openModal(event);
+  }
+  onWatchedClick() {
+    // клік на кнопку Watched
+  }
+  onQueueClick() {
+    // клік на Queue
   }
 
   setPrevNextButtons(data) {
