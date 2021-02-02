89d67d34979749999cf30f116623858039ea0506
RomanKonopelko <romank761@gmail.com>
2021-02-02 17:01:07 +0200

    parallax opt

--- src/js/0parallax.js
+++ src/js/0parallax.js
@@ -1,16 +1,22 @@
 function parallax(event) {
   let speed = layer.getAttribute('data-speed');
-  let test;
+  let item;
   layer.style.transform = `translateX(${
     (event.clientX * speed) / 500
   }px) translateY(${(event.clientY * speed) / 1000}px)`;
-  test = [...title].forEach(el => {
+  title.style.transform = `translateX(${
+    (-event.clientX * 10) / 300
+  }px) translateY(${(-event.clientY * 6) / 300}px)`;
+
+  item = [...title2].forEach(el => {
     el.style.transform = `translateX(${
-      (-event.clientX * 6) / 300
-    }px) translateY(${(-event.clientY * 6) / 300}px)`;
+      (-event.clientX * 4) / 800
+    }px) translateY(${(-event.clientY * 2) / 800}px)`;
   });
 }
 
 const layer = document.querySelector('.layer__bg');
-const title = document.querySelectorAll('.header__item');
+const title = document.querySelector('.parallax-logo');
+const title2 = document.querySelectorAll('.parallax-item');
+
 document.addEventListener('mousemove', parallax);
